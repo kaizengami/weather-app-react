855a679f0b1c01ad6647657244e6c11c584ef85f
Olexander <43763059+kaizengami@users.noreply.github.com>
2019-05-14 13:06:00 +0300

    add production build

---
+++ build/precache-manifest.954b2743cfd887b23dd9efd28b6a4b15.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58aa6d4e7a14d5eee5f9e69210d8ffa",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "4ce319be7b9b76f55283",
+    "url": "/weather-app-react/static/css/main.f4902980.chunk.css"
+  },
+  {
+    "revision": "f09ed38268680c9d64df",
+    "url": "/weather-app-react/static/js/2.edd34329.chunk.js"
+  },
+  {
+    "revision": "4ce319be7b9b76f55283",
+    "url": "/weather-app-react/static/js/main.5ed6943d.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "9245b75764ac2629fef55099dd522a47",
+    "url": "/weather-app-react/static/media/Campton-Bold.9245b757.woff"
+  },
+  {
+    "revision": "7199e521964676d54fe755646a65cbd0",
+    "url": "/weather-app-react/static/media/Campton-Light.7199e521.woff"
+  },
+  {
+    "revision": "4bd4de2de94d5bbe5165c0add29a21e5",
+    "url": "/weather-app-react/static/media/Campton-Medium.4bd4de2d.woff"
+  },
+  {
+    "revision": "f7f92207f1eaa4bd295b23e14e662adb",
+    "url": "/weather-app-react/static/media/Campton-SemiBold.f7f92207.woff"
+  },
+  {
+    "revision": "43f61d16a75eb17f616e4d2b18a0cb9f",
+    "url": "/weather-app-react/static/media/bg-day-mobile.43f61d16.png"
+  },
+  {
+    "revision": "cfd5517e179aa3c25385b978165ac821",
+    "url": "/weather-app-react/static/media/bg-night-mobile.cfd5517e.png"
+  },
+  {
+    "revision": "ea2b56cc60df3998593c49770aa7aed9",
+    "url": "/weather-app-react/static/media/bg-night-pc.ea2b56cc.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app-react/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app-react/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app-react/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app-react/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-app-react/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
